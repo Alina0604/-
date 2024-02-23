18dc973a366a1269455bcd5f42173e6a27333f43
Alina0604 <159549715+Alina0604@users.noreply.github.com>
2024-02-23 22:16:59 +0300

    Update крестики-нолики.py

--- крестики-нолики.py
+++ крестики-нолики.py
@@ -24,19 +24,16 @@ def new_game():
         y = i % 3 * 100
         games.create_rectangle(x, y, x + 100, y + 100, width=3, outline='#A5A5A5', fill='#CCCCCC', activefill='#FFFAFA')
 
-
 def add_x(column, row):
     x = 10 + 100 * column
     y = 10 + 100 * row
     games.create_line(x, y, x + 80, y + 80, width=7, fill='#0000FF')
     games.create_line(x, y + 80, x + 80, y, width=7, fill='#0000FF')
 
-
 def add_o(column, row):
     x = 10 + 100 * column
     y = 10 + 100 * row
     games.create_oval(x, y, x + 80, y + 80, width=7, outline='#FF0000')
-
 
 def click(event):
     if run_game:
@@ -54,7 +51,6 @@ def click(event):
                     end_game()
     else:
         new_game()
-
 
 def bot_move():
     index = None
@@ -97,7 +93,6 @@ def bot_move():
     row = index // 3
     add_o(colum, row)
 
-
 def winner():
     global win, you_win, bot_win, draw_game
     variants = []
@@ -116,7 +111,6 @@ def winner():
         win = 'Ничья'
     return win
 
-
 def creat_win_line():
     for i in combinations:
         win_line = (condition[i[0]], condition[i[1]], condition[i[2]])
@@ -126,7 +120,6 @@ def creat_win_line():
             games.create_line((index_start % 3) * 100 + 50, (index_start // 3) * 100 + 50, (index_end % 3) * 100 + 50, (index_end // 3) * 100 + 50, width=12, fill='#FF00FF')
             games.update()
             break
-
 
 def end_game():
     global run_game
@@ -139,7 +132,6 @@ def end_game():
     games.delete("all")
     games.create_text(120, 110, text=f"Ты выиграл - {you_win}\nБот выиграл - {bot_win}\nНичья  - {draw_game} ", font=("Hack", 20))
     games.create_text(150, 260, text=f"Нажми для продолжения", font=("Arial", 15))
-
 
 games.bind('<Button-1>', click)
 new_game()
